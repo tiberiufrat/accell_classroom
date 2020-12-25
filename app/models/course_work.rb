8495c1194d37b05eef04dfa8c8c9d149dcc4e9e8
Tiberiu Frățilă <tiberiu.fratila@gmail.com>
2020-12-25 19:17:19 +0200

    add translated_work_type as model method

--- app/models/course_work.rb
+++ app/models/course_work.rb
@@ -4,4 +4,17 @@ class CourseWork < ApplicationRecord
   has_many :youtube_videos, as: :youtube_videoable
   has_many :links, as: :linkable
   has_many :forms, as: :formable
+
+  def translated_work_type
+    case work_type
+    when 'ASSIGNMENT'
+      I18n.t('course_work.work_type.assignment')
+    when 'SHORT_ANSWER_QUESTION'
+      I18n.t('course_work.work_type.short_question')
+    when 'MULTIPLE_CHOICE_QUESTION'
+      I18n.t('course_work.work_type.multiple_choice')
+    else
+      I18n.t('course_work.work_type.unspecified')
+    end
+  end
 end
