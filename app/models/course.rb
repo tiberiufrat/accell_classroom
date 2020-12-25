b47e2df6b55a99e0e48d59c061708335dd11375e
Tiberiu Frățilă <tiberiu.fratila@gmail.com>
2020-12-25 16:00:05 +0200

    move status badges to a model function

--- app/models/course.rb
+++ app/models/course.rb
@@ -3,4 +3,21 @@ class Course < ApplicationRecord
   has_many :announcements, dependent: :destroy
   has_many :course_work_materials, dependent: :destroy
   has_many :course_works, dependent: :destroy
+
+  def status_badge
+    case course_state
+    when "ACTIVE"
+         "<span class=\"badge badge-success\">#{ I18n.t('courses.status.active') }</span>"
+    when "ARCHIVED"
+      "<span class=\"badge badge-warning\">#{ I18n.t('courses.status.archived') }</span>"
+    when "PROVISIONED"
+      "<span class=\"badge badge-light\">#{ I18n.t('courses.status.provisioned') }</span>"
+    when "DECLINED"
+      "<span class=\"badge badge-danger\">#{ I18n.t('courses.status.declined') }</span>"
+    when "SUSPENDED"
+      "<span class=\"badge badge-secondary\">#{ I18n.t('courses.status.suspended') }</span>"
+    else
+      "<span class=\"badge badge-dark\">#{ I18n.t('courses.status.unknown') }</span>"
+    end
+  end
 end
