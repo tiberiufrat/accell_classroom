d0abb3ed45cb46adf6f5575f855bd408159b23d2
Tiberiu Frățilă <tiberiu.fratila@gmail.com>
2020-12-26 10:32:07 +0200

    add dependent: :destroy to User model's associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,9 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable,
          :omniauthable, omniauth_providers: [:google_oauth2]
 
-  has_many :courses
-  has_many :reports
-  has_many :activities, through: :reports
+  has_many :courses, dependent: :destroy
+  has_many :reports, dependent: :destroy
+  has_many :activities, through: :reports, dependent: :destroy
 
   def self.create_from_provider_data(provider_data)
     where(provider: provider_data.provider, uid: provider_data.uid).first_or_create do |user|
