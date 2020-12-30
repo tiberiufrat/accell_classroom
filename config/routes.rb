e693e6afaba811bec6585ecc037a5c57ee9a1443
Tiberiu Frățilă <tiberiu.fratila@gmail.com>
2020-12-30 19:21:59 +0200

    add about method to home controller

--- config/routes.rb
+++ config/routes.rb
@@ -19,5 +19,6 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { omniauth_callbacks: 'omniauth' }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'home#index'
+  get '/about', to: 'home#about', as: 'about'
   mount Sidekiq::Web => '/sidekiq'
 end
